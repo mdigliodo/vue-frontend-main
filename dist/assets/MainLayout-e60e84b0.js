733a2f389e9ecc22262c07485d2a04bcff114b1c
Mateo <mdigliodo@gmail.com>
2025-07-22 11:56:48 +0200

    chore: remove dist folder

---
+++ dist/assets/MainLayout-e60e84b0.js
@@ -0,0 +1,1 @@
+import{o as e,d as t,r as n}from"./index-2457fc58.js";const l=Object.assign({name:"MainLayout"},{__name:"MainLayout",setup(o){return(a,r)=>(e(),t("main",null,[n(a.$slots,"default")]))}});export{l as default};
